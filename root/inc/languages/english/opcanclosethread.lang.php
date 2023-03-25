lairdshaw/OP-can-close-thread

commit a8e8b9a9cd82a06f4e1accc0d9678e01d8987d15

Close a mistaken-mod-log entry hole
Instead of mistakenly creating an entry in the moderation log, show a
no-permission error.

root/inc/plugins/opcanclosethread.php
@@ -525,11 +525,14 @@ function opcanclosethread_hookin__moderation_start() {
 			error($lang->error_thread_deleted, $lang->error);
 		}
 
-		if ($thread['closed'] == 1 && $thread['opcct_closed_by_author'] == 1) {
+		if ($thread['closed'] == 1) {
+			if ($thread['opcct_closed_by_author'] != 1) {
+				error_no_permission();
+			}
 			$openclose = $lang->opened;
 			$redirect = $lang->redirect_openthread;
 			$moderation->open_threads($tid);
-		} else if ($thread['closed'] != 1) {
+		} else {
 			$openclose = $lang->closed;
 			$redirect = $lang->redirect_closethread;
 			$fields = array('closed' => 1, 'opcct_closed_by_author' => 1);

commit 014e448dbdb8054c85a4dd9d20296d50f536fd68

Correct the permission error introduced in a8e8b9a9c

root/inc/languages/english/opcanclosethread.lang.php
@@ -3,6 +3,7 @@
 $l['opcct_err_no_close_right_in_forum'  ] = 'You do not have the right to close threads in this forum.';
 $l['opcct_err_no_close_right_not_author'] = 'You may not close this thread as you are not its author.';
 $l['opcct_err_no_thread_closed_by_op'   ] = 'You cannot post replies in this thread because it has been closed by its author.';
+$l['opcct_err_not_closed_by_author'     ] = 'You may not (re)open this thread because it was closed by a moderator, not you.';
 
 $l['opcct_open_thread'                  ] = 'Open Thread';
 $l['opcct_close_thread'                 ] = 'Close Thread';

root/inc/plugins/opcanclosethread.php
@@ -514,7 +514,7 @@ function opcanclosethread_hookin__moderation_start() {
 
 		if ($thread['closed'] == 1) {
 			if ($thread['opcct_closed_by_author'] != 1) {
-				error_no_permission();
+				error($lang->opcct_err_not_closed_by_author, $lang->error);
 			}
 			$openclose = $lang->opened;
 			$redirect = $lang->redirect_openthread;